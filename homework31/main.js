d8f1ad9d2ff70f546482580bb5660cd42cdc6740
ria333454 <mwvaoake@gmail.com>
2026-08-16 20:26:45 +0300

    Add files via upload

---
+++ homework31/main.js
@@ -0,0 +1,31 @@
+//Задача 1
+
+const firstName = "Иван";
+const lastName = "Пупкин";
+const isStudent = true;
+
+//Задача 2
+
+let age = 20;
+let currentYear = 2026;
+let birthYear = currentYear - age;
+
+//Задача 3
+
+console.log(`Меня зовут ${firstName} ${lastName}, мне ${age} лет. Я ученик курса:${isStudent}`);
+
+//Задача 4
+
+let a = '123';
+
+let b = +'456';
+
+let c = Number('789');
+
+let d = Boolean(0);
+
+let e = Boolean('');
+
+let result = a + b + c + d + e;
+
+console.log(result)
